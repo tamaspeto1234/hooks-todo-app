c1d607af8fe328061cbf7bc8f347333269125f7e
tamaspeto1234 <tamas.peto.jobs@gmail.com>
2020-12-02 19:36:42 +0100

    Updates

---
+++ precache-manifest.6f6574c805ff6506f53d9d363badb7a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3946c366177d2d28c7c437015d962867",
+    "url": "/hooks-todo-app/index.html"
+  },
+  {
+    "revision": "992d831f8ebc1dcc7fce",
+    "url": "/hooks-todo-app/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "e5254039d8403f9b14e0",
+    "url": "/hooks-todo-app/static/js/2.3be25a88.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/hooks-todo-app/static/js/2.3be25a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992d831f8ebc1dcc7fce",
+    "url": "/hooks-todo-app/static/js/main.0557c051.chunk.js"
+  },
+  {
+    "revision": "2c62e8116ae72807925d",
+    "url": "/hooks-todo-app/static/js/runtime-main.841cac6d.js"
+  }
+]);
